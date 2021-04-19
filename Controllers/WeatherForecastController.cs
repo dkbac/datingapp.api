584eb7a97b5229c0ec400c5a26a124c77c130937
khacbac.dau <khacbac.dau@vn.wilmar-intl.com>
2021-04-19 16:54:08 +0700

    Initial Commit Dating App

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly DataContext _context;
+
+        public WeatherForecastController(
+            ILogger<WeatherForecastController> logger,
+            DataContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        // [HttpGet]
+        // public IEnumerable<WeatherForecast> Get()
+        // {
+        //     var rng = new Random();
+        //     return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //     {
+        //         Date = DateTime.Now.AddDays(index),
+        //         TemperatureC = rng.Next(-20, 55),
+        //         Summary = Summaries[rng.Next(Summaries.Length)]
+        //     })
+        //     .ToArray();
+        // }
+
+        [HttpGet]
+        public IActionResult GetValues()
+        {
+            var values = _context.Values.ToList();
+
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetValues(int id)
+        {
+            var value = _context.Values.FirstOrDefault(e => e.Id == id);
+
+            return Ok(value);
+        }
+    }
+}
